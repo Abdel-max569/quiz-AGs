5a28fa776cd92729f0bdd850ef042f26959db194
abdel <abdelagouda24@gmail.com>
2025-11-26 17:33:55 +0000

    v2

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let question = document.querySelector(".question_current");
 let propositions = document.querySelectorAll(".propo");
 let explication = document.querySelector(".explication");
-let url = "quiz.json";
+let url = "";
 let list_question = [];
 let indexQuestionActuelle = 0;
 let nuber_actu = document.querySelector(".nuber_actu");
@@ -12,55 +12,106 @@ let recommencer = document.querySelector(".btn-recommencer")
 let victoire = 0;
 
 // -------------------------------------
+card_all = document.querySelector(".card-all");
 card_python= document.querySelector(".card-python");
 card_java= document.querySelector(".card-java");
 card_php= document.querySelector(".card-php");
 card_js= document.querySelector(".card-js");
-//console.log(card_python,card_js)
 
-card_python.addEventListener("click", () => {
-    url = "quiz_python.json";
+let principal = document.querySelector(".principal");
+let container = document.querySelector(".container");
+// console.log(container);
+
+function chargerDonneesQuiz() {
+    if (url === "") {
+        return; 
+    }
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            list_question = data;
+            total.textContent = list_question.length;
+            chargerQuestion(indexQuestionActuelle);
+        })
+}
+
+function putPrincipalNone(){
+    principal.style.display="none";
+}
+
+function putQuizBlock(){
+    container.style.display="block";
+}
+
+card_all.addEventListener("click", () => {
+    url = "quiz.json";
     indexQuestionActuelle = 0;
-    alert("Quiz Python sélectionné !");
+    chargerDonneesQuiz()
+    putPrincipalNone()
+    putQuizBlock()
+    
+});
+card_python.addEventListener("click", () => {
+    url = "python.json";
+    indexQuestionActuelle = 0;
+    chargerDonneesQuiz()
+    putPrincipalNone()
+    putQuizBlock()
 });
 
 card_java.addEventListener("click", () => {
-    url = "quiz_java.json";
+    url = "java.json";
     indexQuestionActuelle = 0;
-    alert("Quiz Java sélectionné !");
+    chargerDonneesQuiz()
+    putPrincipalNone()
+    putQuizBlock()
+    
 });
 card_php.addEventListener("click", () => {
-    url = "quiz_php.json";
+    url = "php.json";
     indexQuestionActuelle = 0;
-    alert("Quiz PHP sélectionné !");
+    chargerDonneesQuiz()
+    putPrincipalNone()
+    putQuizBlock()
+  
 });
 
 card_js.addEventListener("click", () => { 
-    url = "quiz_php.json";
+    url = "js.json";
     indexQuestionActuelle = 0;
-    alert("Quiz PHP sélectionné !");
+    chargerDonneesQuiz()
+    putPrincipalNone()
+    putQuizBlock()
 });
 
 
-//--------------------------------------
-
-// const sonVictoire = new Audio('victoire.wav');
+//----------------SONG----------------------
+// const sonVictoire = new Audio('victoire.mp3');
 // sonVictoire.volume = 0.8;
 
 // const sonDefaite = new Audio('defaite.wav');
 // sonDefaite.volume = 0.6;
 
-// function victoire() {
-//     sonVictoire.play();
+// function victoire1() {
+//     try {
+//         sonVictoire.currentTime = 0; 
+//         sonVictoire.play();
+//     } catch (error) {
+//         console.error("Erreur lors de la lecture du son de victoire:", error);
+//     }
 // }
-
 // function defaite() {
 //     sonDefaite.play();
 // }
 
+//----------------END SONG-----------
 
 
 
+
+
+//----------------PASSER-RECOMMENCER-----------
 passer.addEventListener("click", () => {
     indexQuestionActuelle++;
     chargerQuestion(indexQuestionActuelle);
@@ -69,30 +120,37 @@ passer.addEventListener("click", () => {
 recommencer.addEventListener("click", ()=>{
     window.location.reload();
 })
-
-
-const timerElement = document.querySelector(".compteur");
-
-function demarrerCompteARebours(secondes) {
-    const miseAJour = () => {
-        if (secondes > 0) {
-            timerElement.textContent = secondes + " s";
-
-            secondes--;
-
-            setTimeout(miseAJour, 1000);
-        } else {
-            indexQuestionActuelle++;
-            chargerQuestion(indexQuestionActuelle);
-
-        }
-    };
-    miseAJour();
-}
+//----------------PASSER-RECOMMENCER-----------
 
 
 
 
+
+//-------------------------TIME-----------------------
+// const timerElement = document.querySelector(".compteur");
+
+// function demarrerCompteARebours(secondes) {
+//     const miseAJour = () => {
+//         if (secondes > 0) {
+//             timerElement.textContent = secondes + " s";
+
+//             secondes--;
+
+//             setTimeout(miseAJour, 1000);
+//         } else {
+//             indexQuestionActuelle++;
+//             chargerQuestion(indexQuestionActuelle);
+
+//         }
+//     };
+//     miseAJour();
+// }
+
+//-------------------------TIME-----------------------
+
+
+
+//
 
 fetch(url)
     .then(response => response.json())
@@ -133,7 +191,8 @@ function checkResponse(checkIndex) {
     if (checkIndex === correctIndex) {
         propositions[checkIndex].style.background = "green";
         victoire++
-        console.log(victoire);
+        //console.log(victoire);
+        victoire1()
     } else {
         propositions[checkIndex].style.background = "red";
         propositions[correctIndex].style.background = "green";
